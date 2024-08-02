4d6b394f3afe731e9a064f3c64ca861684140eaa
Matthew <davewibisono@gmail.com>
2024-08-02 16:10:10 +0500

    header, existing pages responsiveness fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
   // References
-  let menuBtn = document.getElementById('menuBtn');
-  let menu = document.getElementById('menu');
-  let header = document.getElementById('header');
-  let hero = document.querySelectorAll('.heroSection');
   const logos = document.querySelector('.clients');
-
-  // Menu Toggle
-  menuBtn.addEventListener('click', function () {
-    if (menu.classList.contains('d-none')) {
-      menu.classList.remove('d-none');
-      menu.classList.add('d-flex');
-    } else {
-      menu.classList.add('d-none');
-    }
-  });
+  const header = document.querySelector('.header');
+  const hero = document.querySelectorAll('.hero');
 
   // Header Background Change On Scroll
-  window.addEventListener('scroll', function () {
-    let heroBottom = Array.from(hero).reduce((minBottom, hero) => {
-      return Math.min(minBottom, hero.getBoundingClientRect().bottom);
-    }, Number.POSITIVE_INFINITY);
-    if (header.getBoundingClientRect().top <= heroBottom) {
-      header.classList.add('bg-main-neo');
-      header.classList.remove('bg-main');
-    } else {
-      header.classList.add('bg-main');
-      header.classList.remove('bg-main-neo');
-    }
-  });
+  if (header) {
+    window.addEventListener('scroll', function () {
+      let heroBottom = Array.from(hero).reduce((minBottom, hero) => {
+        return Math.min(minBottom, hero.getBoundingClientRect().bottom);
+      }, Number.POSITIVE_INFINITY);
+  
+      if (header.getBoundingClientRect().top <= heroBottom) {
+        header.classList.add('bg-main-neo');
+        header.classList.remove('bg-main');
+      } else {
+        header.classList.add('bg-main');
+        header.classList.remove('bg-main-neo');
+      }
+    });
+  } else {
+    console.error('Header element with class "header" not found.');
+  }
 
-  // Sponsers Slider
+  if (logos) {
+  // Sponsors Slider
   let startX;
   let scrollLeft;
 
@@ -55,6 +49,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const walk = (x - startX) * 2; // Scroll-fast
     logos.scrollLeft = scrollLeft - walk;
   });
+} else {
+  
+}
 
   // Swiper Init
   const swiper = new Swiper(".swiper", {
